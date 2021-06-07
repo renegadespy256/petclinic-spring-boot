d6eecba51ab47d9c57639cf6b1dc400725256606
renegadespy256 <bhattacharjeesoham5@gmail.com>
2021-06-07 15:32:50 +0530

    initial-commit

---
+++ src/main/java/com/soham/petclinicspringboot/PetclinicSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.soham.petclinicspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetclinicSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soham/petclinicspringboot/PetclinicSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soham.petclinicspringboot;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
