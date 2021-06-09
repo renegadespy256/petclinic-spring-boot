edc3f18ca46cc68fabb3744b53b1818f3888d268
renegadespy256 <bhattacharjeesoham5@gmail.com>
2021-06-10 00:33:31 +0530

    updated /owners view layer.

--- src/main/java/com/soham/petclinicspringboot/controller/OwnerController.java
+++ src/main/java/com/soham/petclinicspringboot/controller/OwnerController.java
@@ -1,14 +1,26 @@
 package com.soham.petclinicspringboot.controller;
 
+import com.soham.petclinicspringboot.services.OwnerService;
+import com.soham.petclinicspringboot.services.map.OwnerServiceMap;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OwnerController {
 
-    @RequestMapping({"/owners","/owners/","/owners/index","/owners/index.html"})
-    public String listOwner(){
+    private final OwnerService ownerService;
 
+    @Autowired
+    public OwnerController(OwnerServiceMap ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"/owners","/owners/","/owners/index","/owners/index.html"})
+    public String listOwner(Model model){
+
+        model.addAttribute("owners",ownerService.findAll());
         return "owners/index";
     }
 }
