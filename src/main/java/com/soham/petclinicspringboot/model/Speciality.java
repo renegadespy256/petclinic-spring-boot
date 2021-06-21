22354baad109726712921304d1684eb3d5b816bf
renegadespy256 <bhattacharjeesoham5@gmail.com>
2021-06-21 20:52:31 +0530

    implemented Speciality entity and updated Vet POJO.

---
+++ src/main/java/com/soham/petclinicspringboot/model/Speciality.java
@@ -0,0 +1,14 @@
+package com.soham.petclinicspringboot.model;
+
+public class Speciality {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
